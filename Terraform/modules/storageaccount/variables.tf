a7850daf235c48f20702834e4805319debff6a82
Omar Lucia <DevopsTest.Nov04@arroyoconsulting.net>
2024-11-25 01:09:12 -0800

    terraform code

--- Terraform/modules/storageaccount/variables.tf
+++ Terraform/modules/storageaccount/variables.tf
@@ -15,11 +15,11 @@ variable "location" {
 variable "prefix" {
   description = "Prefix for all terraform resources"
   type        = string
-  default = "tf"
+  default = "terraform"
 }
 
 variable "resources_name" {
-  description = "Storage account name"
+  description = "name for all terraform resources"
   type        = string
   default = "omarlucia"
 }
@@ -35,16 +35,5 @@ variable "account_tier" {
   
 }
 variable "account_replication_type" {
-  
-}
-
-## KeyVault variables
-variable "sku_name" {
-  
-}
-
-variable "object_id" {
-  description = "The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault." # Need to change
-  type = string
   
 }
